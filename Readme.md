88b893239cae0acc3dde14555141199e0c44a584
Jannis Leon Jung <jannis@bbhj-jung.de>
2024-03-12 15:41:24 +0100

    Removed nannou and added bevy

--- Readme.md
+++ Readme.md
@@ -1,1 +1,6 @@
 # General Relativity simulation in Rust
+
+
+## Future plans
+- Differ between simulator and bevy modules (gr_sim and gr_sim_bevy)
+- Output multiple modes (e.g. graphics simulation and output file mode)
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
-use nannou::prelude::*;
-
-struct Model{}
-
-fn model(_app: &App) -> Model {
-    Model {}
-}
-
-fn update(_app: &App, _model: &mut Model, _update: Update) {
-
-}
-
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(PURPLE);
-}
+use bevy::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).simple_window(view).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run();
 }
